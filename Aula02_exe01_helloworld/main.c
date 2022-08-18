8a7c5a7202d8076e0e1ec8b47f82f740d217a1d1
vinicius <2201268@escolas.Anchieta.br>
2022-08-17 21:42:09 -0300

    first commit

---
+++ Aula02_exe01_helloworld/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Hello World!\n");
+	//scanf()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	if(1 == 1){
+		printf("deu certo");
+	} else {
+		printf("tu é burro");
+	}
+	
+	int a = 1;
+	
+	while(a <= 5){
+		printf("\npassando");
+		//printf(a);
+		a = a + 1;
+	}
+	return 0;
+}
